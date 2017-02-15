27407adf8eacfda8ecc695b8914b98bbe9ae9ad7
Fck-r-sns <fck.r.sns@gmail.com>
2017-02-15 21:31:56 +0300

    Базовый класс для событий

---
+++ Assets/Scripts/EventBus/Event.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EventBus
+{
+    public enum EventType
+    {
+        TestEvent
+    }
+
+    public class Event
+    {
+        public EventType type;
+        public int address;
+
+        public override string ToString()
+        {
+            return "Event(type=" + type + ";address=" + address + ")";
+        }
+    }
+}
