fffadb5d85323bd513221835e4c3732f7f9af95a
Fck-r-sns <fck.r.sns@gmail.com>
2017-02-15 21:43:08 +0300

    Зарезервированные адреса

--- Assets/Scripts/EventBus/Defines.cs
+++ Assets/Scripts/EventBus/Defines.cs
@@ -7,5 +7,12 @@ namespace EventBus
     public static class Defines
     {
         public const int BROADCAST_ADDRESS = -1;
+
+        // reserved addresses
+        public const int PLAYER_ADDRESS = 1;
+        public const int MONSTER_ADDRESS = 2;
+        public const int TEST_ADDRESS = 42;
+
+        public const int FIRST_FREE_ADDRESS = 100;
     }
 }
