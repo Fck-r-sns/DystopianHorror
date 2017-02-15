5f2627a0525a27ab682d4eb6a7435c3d1ecda138
Fck-r-sns <fck.r.sns@gmail.com>
2017-02-15 21:33:07 +0300

    Тестовые скрипты

---
+++ Assets/Scripts/EventBus/TestPublisher.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPublisher : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EventBus.Dispatcher.SendEvent(
+            new EventBus.Event
+            {
+                type = EventBus.EventType.TestEvent,
+                address = 42
+            }
+            );
+    }
+}
---
+++ Assets/Scripts/EventBus/TestSubscriber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using EventBus;
+using UnityEngine;
+
+public class TestSubscriber : MonoBehaviour, EventBus.IEventSubscriber {
+
+    public void OnReceived(EventBus.Event e)
+    {
+        Debug.Log("Event received: " + e);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        EventBus.Dispatcher.Subscribe(EventBus.EventType.TestEvent, 42, gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
