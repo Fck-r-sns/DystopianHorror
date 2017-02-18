21a7fd287888f8b9888c7da1fb08f84c72d98f65
Fck-r-sns <fck.r.sns@gmail.com>
2017-02-18 18:46:13 +0300

    Управление дверь с помощью событий

---
+++ Assets/Scripts/DoorTrigger_Receiver.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+using EventBus;
+using System;
+
+public class DoorTrigger_Receiver : MonoBehaviour, IEventSubscriber {
+
+    private Door door;
+    private int address = AddressProvider.GetFreeAddress();
+
+    public void OnReceived(EBEvent e)
+    {
+        switch (e.type)
+        {
+            case EBEventType.TestDoorTriggerEntered:
+                door.open();
+                break;
+            case EBEventType.TestDoorTriggerExited:
+                door.close();
+                break;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        door = GetComponent<Door>();
+        Dispatcher.Subscribe(EBEventType.TestDoorTriggerEntered, address, gameObject);
+        Dispatcher.Subscribe(EBEventType.TestDoorTriggerExited, address, gameObject);
+    }
+}
---
+++ Assets/Scripts/DoorTrigger_Sender.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using EventBus;
+
+public class DoorTrigger_Sender : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        Dispatcher.SendEvent(new EBEvent { type = EBEventType.TestDoorTriggerEntered });
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Dispatcher.SendEvent(new EBEvent { type = EBEventType.TestDoorTriggerExited });
+    }
+}
