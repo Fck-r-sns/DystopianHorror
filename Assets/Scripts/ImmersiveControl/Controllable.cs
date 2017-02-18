63917c122f155e297d832537eb7a58757260b74d
Fck-r-sns <fck.r.sns@gmail.com>
2017-02-18 20:05:13 +0300

    Controllable

---
+++ Assets/Scripts/ImmersiveControl/Controllable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Immersive
+{
+
+    public interface Controllable
+    {
+        void OnHoverOn();
+        void OnHoverOut();
+        void OnAcquire();
+        void OnRelease();
+        void OnForceApplied();
+    }
+
+}
