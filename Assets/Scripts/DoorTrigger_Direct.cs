ca7ef89fd56b0d68916960b6bcf9660bcb3c08b5
Fck-r-sns <fck.r.sns@gmail.com>
2017-02-18 18:09:36 +0300

    Триггер двери с прямой ссылкой

---
+++ Assets/Scripts/DoorTrigger_Direct.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger_Direct : MonoBehaviour {
+
+    [SerializeField]
+    Door door;
+
+    void OnTriggerEnter(Collider other)
+    {
+        door.open();
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        door.close();
+    }
+}
