500a4e0e3c9d0877cfb90c3bd7cdcf338ef73d65
Fck-r-sns <fck.r.sns@gmail.com>
2017-03-04 15:59:05 +0300

    Скрипт для преследуемого объекта

---
+++ Assets/Scripts/Monster/Prey.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using EventBus;
+
+public class Prey : MonoBehaviour {
+
+    [SerializeField]
+    private float waypointSpawnPeriod_s = 1.0f;
+
+    private float lastSpawnTime = 0.0f;
+
+	void Start () {
+        lastSpawnTime = Time.time;
+	}
+	
+	void Update () {
+		if (Time.time - lastSpawnTime > waypointSpawnPeriod_s)
+        {
+            CreateWaypoint();
+            lastSpawnTime += waypointSpawnPeriod_s;
+        }
+	}
+
+    private void CreateWaypoint()
+    {
+        Dispatcher.SendEvent(new NewWaypointEvent(transform.position, Time.time));
+    }
+}
