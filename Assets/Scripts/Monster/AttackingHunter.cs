224339463be0a5a91c6a371960d0f49e75202634
Fck-r-sns <fck.r.sns@gmail.com>
2017-03-04 18:26:23 +0300

    Атака по прямой

---
+++ Assets/Scripts/Monster/AttackingHunter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackingHunter : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private float speed = 10.0f;
+
+    void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        direction.y = 0;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+    }
+}
