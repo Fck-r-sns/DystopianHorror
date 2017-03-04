9f902740dae4f4a8c5b2a70a6b7970ff9de50526
Fck-r-sns <fck.r.sns@gmail.com>
2017-03-04 15:50:40 +0300

    NewWaypointEvent

---
+++ Assets/Scripts/Events/NewWaypointEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using EventBus;
+
+public class NewWaypointEvent : EBEvent {
+
+    public readonly Vector3 position;
+    public readonly float time;
+    
+    public NewWaypointEvent(Vector3 position, float time)
+    {
+        this.type = EBEventType.NewWaypointCreated;
+        this.position = position;
+        this.time = time;
+    }
+
+}
