4a3184eaf271d99bcc768c0a18e3ae9cccce5710
Fck-r-sns <fck.r.sns@gmail.com>
2017-03-04 16:18:58 +0300

    Преследующий объект получает новые вейпоинты

---
+++ Assets/Scripts/Monster/Hunter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using EventBus;
+using System;
+
+public class Hunter : MonoBehaviour, IEventSubscriber {
+
+    private int address = AddressProvider.GetFreeAddress();
+    private Queue<Waypoint> waypointsQueue = new Queue<Waypoint>();
+
+    public void OnReceived(EBEvent e)
+    {
+        if (e.type == EBEventType.NewWaypointCreated)
+        {
+            waypointsQueue.Enqueue((e as NewWaypointEvent).waypoint);
+        }
+    }
+
+    void Start () {
+        Dispatcher.Subscribe(EBEventType.NewWaypointCreated, address, gameObject);
+	}
+
+    void OnDestroy()
+    {
+        Dispatcher.Unsubscribe(EBEventType.NewWaypointCreated, address);
+    }
+
+    void Update () {
+		
+	}
+}
