e07eb7871fb4c619ad151539a85a7d772eefda83
Fck-r-sns <fck.r.sns@gmail.com>
2017-03-04 19:17:17 +0300

    Проверка видимости камерой

---
+++ Assets/Scripts/Monster/CameraVisibilityChecker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraVisibilityChecker : MonoBehaviour {
+
+    [SerializeField]
+    private Camera camera;
+
+    private Collider targetCollider;
+
+    private bool visible = false;
+
+    public bool isVisible()
+    {
+        return visible;
+    }
+
+    private void Awake()
+    {
+        targetCollider = GetComponent<Collider>();
+    }
+
+    private void Update () {
+        visible = CheckVisibility();
+	}
+
+    private bool CheckVisibility()
+    {
+        Plane[] cameraPlanes = GeometryUtility.CalculateFrustumPlanes(camera);
+        Bounds targetBounds = targetCollider.bounds;
+        return GeometryUtility.TestPlanesAABB(cameraPlanes, targetBounds);
+    }
+}
