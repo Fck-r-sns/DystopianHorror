17512c8a8d6327967a7f9b38115002dd6ffbf493
Fck-r-sns <fck.r.sns@gmail.com>
2017-03-05 18:11:44 +0300

    Отрисовка вейпоинтов для отладки

---
+++ Assets/Scripts/Monster/WaypointDrawer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+using EventBus;
+
+public class WaypointDrawer : MonoBehaviour, IEventSubscriber
+{
+
+    private int address = AddressProvider.GetFreeAddress();
+
+    public void OnReceived(EBEvent e)
+    {
+        if (e.type == EBEventType.NewWaypointCreated)
+        {
+            DrawWaypoint((e as NewWaypointEvent).waypoint.position);
+        }
+    }
+
+    private void Awake()
+    {
+        Dispatcher.Subscribe(EBEventType.NewWaypointCreated, address, gameObject);
+    }
+
+    private void DrawWaypoint(Vector3 position)
+    {
+        Debug.DrawRay(position, Vector3.up, Color.black, 10.0f);
+    }
+
+}
