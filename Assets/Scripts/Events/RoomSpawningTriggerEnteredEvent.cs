1068a00fa5f59239158ba45e664e88b844b781da
Fck-r-sns <fck.r.sns@gmail.com>
2017-03-08 00:12:54 +0300

    RoomSpawningTriggerEnteredEvent

---
+++ Assets/Scripts/Events/RoomSpawningTriggerEnteredEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using EventBus;
+
+public class RoomSpawningTriggerEnteredEvent : EBEvent
+{
+
+    public readonly string roomsManagerId;
+    public readonly Vector3 rootOffset;
+    public readonly Vector3 rootRotation;
+
+    public RoomSpawningTriggerEnteredEvent(string roomsManagerId, Vector3 rootOffset, Vector3 rootRotation)
+    {
+        this.roomsManagerId = roomsManagerId;
+        this.rootOffset = rootOffset;
+        this.rootRotation = rootRotation;
+    }
+
+}
