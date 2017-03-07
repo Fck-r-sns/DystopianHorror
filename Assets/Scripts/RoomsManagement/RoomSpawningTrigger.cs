89803950d82dd1cd0b43b106255a5b5a3aa92bea
Fck-r-sns <fck.r.sns@gmail.com>
2017-03-07 18:45:18 +0300

    Скрипты для динамической загрузки сцен

---
+++ Assets/Scripts/RoomsManagement/RoomSpawningTrigger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomSpawningTrigger : MonoBehaviour
+{
+
+    private string lastSceneName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Scene room = RoomsManager.GetInstance().GetRandomRoom();
+        StartCoroutine(MoveScene(room));
+        lastSceneName = room.name;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (lastSceneName != null)
+        {
+            RoomsManager.GetInstance().UnloadRoom(lastSceneName);
+            lastSceneName = null;
+        }
+    }
+
+    private IEnumerator MoveScene(Scene scene)
+    {
+        while (!scene.isLoaded)
+        {
+            yield return null;
+        }
+        GameObject root = scene.GetRootGameObjects()[0];
+        root.transform.position = new Vector3(0, 0, 10);
+        root.SetActive(true);
+    }
+
+}
---
+++ Assets/Scripts/RoomsManagement/RoomsManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomsManager
+{
+
+    private static RoomsManager instance;
+    private List<string> rooms = new List<string>();
+
+    private RoomsManager()
+    {
+        RegisterRoom("Room1");
+        RegisterRoom("Room2");
+        RegisterRoom("Room3");
+    }
+
+    public static RoomsManager GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new RoomsManager();
+        }
+        return instance;
+    }
+
+    public void RegisterRoom(string sceneName)
+    {
+        rooms.Add(sceneName);
+    }
+
+    public Scene GetRandomRoom()
+    {
+        int index = Random.Range(0, rooms.Count);
+        string sceneName = rooms[index];
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+        return SceneManager.GetSceneByName(sceneName);
+    }
+
+    public void UnloadRoom(string sceneName)
+    {
+        if (SceneManager.GetSceneByName(sceneName).isLoaded)
+        {
+            SceneManager.UnloadSceneAsync(sceneName);
+        }
+    }
+
+}
