a91a0330e274c9422e3bc116532e58b340ce0421
Fck-r-sns <fck.r.sns@gmail.com>
2017-03-09 12:19:27 +0300

    Патрулирование по вейпоинтам

--- Assets/Scripts/Monster/NavMeshMovement.cs
+++ Assets/Scripts/Monster/NavMeshMovement.cs
@@ -7,6 +7,18 @@ using UnityEngine.AI;
 public class NavMeshMovement : MonoBehaviour
 {
 
+    private enum Direction
+    {
+        Forward = +1,
+        Backward = -1
+    }
+
+    private enum State
+    {
+        Patrol,
+        Attack
+    }
+
     [SerializeField]
     private Transform[] waypoints;
 
@@ -16,58 +28,83 @@ public class NavMeshMovement : MonoBehaviour
     [SerializeField]
     private LayerMask layerMask = -1;
 
+    private static float WAYPOINT_PASS_DISTANCE = 1.0f;
+
     private NavMeshAgent navMeshAgent;
+    private Direction direction = Direction.Forward;
+    private State state = State.Patrol;
+    private Transform currentTarget;
+    private int currentWaypointIndex = -1;
 
     // Use this for initialization
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
-        //layerMask = LayerMask.GetMask("Architecture") | LayerMask.GetMask("Player");
-        Debug.Log(layerMask.value);
+        currentTarget = waypoints[3];
+        currentWaypointIndex = 3;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Ray ray = new Ray(transform.position, mainTarget.position - transform.position);
-        RaycastHit hit;
-        Vector3 target;
-        if (Physics.Raycast(ray, out hit, float.MaxValue, layerMask))
+        //Ray ray = new Ray(transform.position, mainTarget.position - transform.position);
+        //RaycastHit hit;
+        //if (Physics.Raycast(ray, out hit, float.MaxValue, layerMask) && hit.transform.gameObject.tag.Equals("Player"))
+        //{
+        //    state = State.Attack;
+        //    currentTarget = mainTarget;
+        //}
+        //else
+        //{
+        //    state = State.Patrol;
+
+        //}
+
+        //if (target != null)
+        //{
+        //    target.y = transform.position.y;
+        //    navMeshAgent.SetDestination(target);
+        //}
+
+        float dst = float.MaxValue;
+        if (currentTarget != null)
         {
-            Debug.DrawLine(transform.position, hit.point, Color.black, 1);
-            if (hit.transform.gameObject.tag.Equals("Player"))
-            {
-                target = mainTarget.position;
-            }
-            else
-            {
-                target = GetNearestWaypoint();
-            }
-        }
-        else
-        {
-            target = GetNearestWaypoint();
+            Vector3 v1 = transform.position;
+            v1.y = 0;
+            Vector3 v2 = currentTarget.position;
+            v2.y = 0;
+            dst = Vector3.Distance(v1, v2);
         }
 
-        if (target != null)
+        if ((dst < WAYPOINT_PASS_DISTANCE))
         {
-            target.y = transform.position.y;
-            navMeshAgent.SetDestination(target);
+            currentWaypointIndex = GetNextWaypoint();
+            currentTarget = waypoints[currentWaypointIndex];
         }
+
+        navMeshAgent.SetDestination(currentTarget.position);
     }
 
-    private Vector3 GetNearestWaypoint()
+    private int GetNextWaypoint()
     {
-
-        float minDistance = float.MaxValue;
-        Vector3 result = transform.position;
-        foreach(Transform waypoint in waypoints)
+        if ((currentWaypointIndex != -1) && (currentTarget != null))
         {
-            float dst = Vector3.Distance(transform.position, waypoint.position);
+            return (currentWaypointIndex + (int)direction + waypoints.Length) % waypoints.Length;
+        }
+        return GetNearestWaypoint();
+    }
+
+    private int GetNearestWaypoint()
+    {
+        float minDistance = float.MaxValue;
+        int result = -1;
+        for (int i = 0; i < waypoints.Length; ++i)
+        {
+            float dst = Vector3.Distance(transform.position, waypoints[i].position);
             if (dst < minDistance)
             {
                 minDistance = dst;
-                result = waypoint.position;
+                result = i;
             }
         }
         return result;
