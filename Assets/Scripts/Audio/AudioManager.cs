f54b9865d899cd5d742a639fbd705745ec7d83b3
Fck-r-sns <fck.r.sns@gmail.com>
2017-04-01 01:33:39 +0300

    AudioManager

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using EventBus;
+using System;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour, IEventSubscriber
+{
+
+    // 2 sources for crossfade
+    [SerializeField]
+    private AudioSource source1;
+
+    [SerializeField]
+    private AudioSource source2;
+
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float maxVolume;
+
+    [SerializeField]
+    private float crossFadeRate = 1.5f;
+
+    [SerializeField]
+    private AudioClip backgroundMusic1;
+
+    [SerializeField]
+    private AudioClip backgroundMusic2;
+
+    private AudioSource[] sources = new AudioSource[2];
+    private int address = AddressProvider.GetFreeAddress();
+    private int currentSourceIndex = 0;
+
+    public void OnReceived(EBEvent e)
+    {
+        changeAudio(backgroundMusic2);
+    }
+
+    void Start () {
+        source1.volume = maxVolume;
+        source2.volume = maxVolume;
+        sources[0] = source1;
+        sources[1] = source2;
+        Dispatcher.Subscribe(EBEventType.HallMovingTriggerEntered, address, gameObject);
+        changeAudio(backgroundMusic1);
+    }
+	
+	void Update () {
+		
+	}
+
+    void changeAudio(AudioClip clip)
+    {
+        if (sources[currentSourceIndex].isPlaying)
+        {
+            StartCoroutine(changeAudioWithFading(clip));
+        }
+        else
+        {
+            currentSourceIndex = 1 - currentSourceIndex;
+            AudioSource s = sources[currentSourceIndex];
+            s.clip = clip;
+            s.Play();
+        }
+    }
+
+    IEnumerator changeAudioWithFading(AudioClip clip)
+    {
+        AudioSource activeSource = sources[currentSourceIndex];
+        AudioSource inactiveSource = sources[1 - currentSourceIndex];
+
+        inactiveSource.volume = 0;
+        inactiveSource.clip = clip;
+        inactiveSource.Play();
+
+        float scaledRate = crossFadeRate * maxVolume;
+        while (activeSource.volume > 0)
+        {
+            float delta = scaledRate * Time.deltaTime;
+            activeSource.volume -= delta;
+            inactiveSource.volume += delta;
+            inactiveSource.volume = Mathf.Min(inactiveSource.volume, maxVolume);
+            yield return null;
+        }
+
+        currentSourceIndex = 1 - currentSourceIndex;
+    }
+}
