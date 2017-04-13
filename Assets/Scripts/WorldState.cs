b26bfbbbaa953813dc77d35f83b97dee81e64264
Fck-r-sns <fck.r.sns@gmail.com>
2017-04-13 13:16:09 +0300

    WorldState

---
+++ Assets/Scripts/WorldState.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+using EventBus;
+
+public class WorldState : MonoBehaviour, IEventSubscriber
+{
+    private int roomsVisited_ = 0;
+    private int madness_ = 0;
+    private int collectiblesFound_ = 0;
+    private bool keyFound_ = false;
+    private int timesCaughtByMonster_ = 0;
+
+    public int roomsVisited {
+        get {
+            return roomsVisited_;
+        }
+    }
+
+    public int madness {
+        get {
+            return madness_;
+        }
+    }
+
+    public int collectiblesFound {
+        get {
+            return collectiblesFound_;
+        }
+    }
+
+    public bool keyFound {
+        get {
+            return keyFound_;
+        }
+    }
+
+    public int timeCaughtByMonster {
+        get {
+            return timesCaughtByMonster_;
+        }
+    }
+
+    public void OnReceived(EBEvent e)
+    {
+        // process events
+    }
+
+    private void Start()
+    {
+        // subscribe
+    }
+
+    private void OnDestroy()
+    {
+        // unsubscribe
+    }
+}
