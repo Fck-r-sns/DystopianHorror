f2671317ee1a0f53b02e618da4acf02bd7f13961
Fck-r-sns <fck.r.sns@gmail.com>
2017-04-14 01:11:10 +0300

    Менеджер предметов и предметы

---
+++ Assets/Scripts/Items/ItemsManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemsManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject keyPrefab;
+
+    [SerializeField]
+    private GameObject[] bookPrefabs;
+
+    [SerializeField]
+    private WorldState worldState;
+
+    private static ItemsManager instance;
+
+    public static ItemsManager GetInstance()
+    {
+        return instance;
+    }
+
+    public GameObject GetItem()
+    {
+        float chance = GetBookChance() * 100;
+        if (Random.Range(0, 99) < chance)
+        {
+            int index = Random.Range(0, bookPrefabs.Length);
+            return Instantiate(bookPrefabs[index]);
+        }
+        return null;
+    }
+
+    void Awake () {
+        instance = this;
+	}
+    
+
+    private float GetBookChance()
+    {
+        //return (100 * WorldState.BOOK_SPAWN_INITIAL_CHANCE - worldState.collectiblesFound) 
+        //    / (100 - worldState.roomsVisited);
+        return 1f;
+    }
+}
--- Assets/Scripts/WorldState.cs
+++ Assets/Scripts/WorldState.cs
@@ -7,7 +7,8 @@ public class WorldState : MonoBehaviour, IEventSubscriber
 
     public const int MIN_MADNESS = 0;
     public const int MAX_MADNESS = 100;
-    
+    public const float BOOK_SPAWN_INITIAL_CHANCE = 0.1f;
+
     private int roomsVisited_ = 0;
     private int madness_ = 0;
     private int collectiblesFound_ = 0;
