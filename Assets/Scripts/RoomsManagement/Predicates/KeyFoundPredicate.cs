97c4b12b22bb06441ec45e853f1c1c14a12efe17
Fck-r-sns <fck.r.sns@gmail.com>
2017-04-14 18:00:24 +0300

    KeyFoundPredicate

---
+++ Assets/Scripts/RoomsManagement/Predicates/KeyFoundPredicate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyFoundPredicate : Predicate
+{
+    [SerializeField]
+    private bool keyFound = false;
+
+    public override bool Check(WorldState world)
+    {
+        return (world.keyFound == keyFound);
+    }
+}
