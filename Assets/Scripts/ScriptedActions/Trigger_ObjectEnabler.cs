c038092ee3413766588031a27cd561d1787b8b88
Fck-r-sns <fck.r.sns@gmail.com>
2017-04-14 17:31:55 +0300

    Хорошая концовка

---
+++ Assets/Scripts/ScriptedActions/Trigger_ObjectEnabler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger_ObjectEnabler : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject target;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        target.SetActive(true);
+    }
+}
