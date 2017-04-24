489af698975c0049075501b2a7eb301430355fe8
Fck-r-sns <fck.r.sns@gmail.com>
2017-04-25 00:43:02 +0300

    Toilet screamer in prologue

---
+++ Assets/Scripts/ScriptedActions/Trigger_ToiletScreamer_Punchline.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger_ToiletScreamer_Punchline : MonoBehaviour
+{
+
+    [SerializeField]
+    private Door door;
+
+    [SerializeField]
+    private AudioSource audioSource;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        door.Close();
+        audioSource.Stop();
+    }
+
+}
---
+++ Assets/Scripts/ScriptedActions/Trigger_ToiletScreamer_Setup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger_ToiletScreamer_Setup : MonoBehaviour
+{
+
+    [SerializeField]
+    private Door door;
+
+    [SerializeField]
+    private AudioSource metalAudioSource;
+
+    [SerializeField]
+    private AudioSource waterAudioSource;
+
+    private bool isTriggered = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isTriggered)
+        {
+            isTriggered = true;
+            metalAudioSource.Play();
+            waterAudioSource.Play();
+            door.SetOpened();
+        }
+    }
+
+}
