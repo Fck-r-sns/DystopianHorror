18bc232bea46abb089e0e62bc391851016e1373a
Fck-r-sns <fck.r.sns@gmail.com>
2017-04-25 00:06:28 +0300

    Monster double trigger fixed

--- Assets/Scripts/Monster/MonsterCatch.cs
+++ Assets/Scripts/Monster/MonsterCatch.cs
@@ -2,8 +2,37 @@
 
 using EventBus;
 
-public class MonsterCatch : MonoBehaviour
+public class MonsterCatch : MonoBehaviour, IEventSubscriber
 {
+
+    private int address = AddressProvider.GetFreeAddress();
+
+    public void OnReceived(EBEvent e)
+    {
+        switch (e.type)
+        {
+            case EBEventType.CaughtByMonster:
+                this.enabled = false;
+                break;
+
+            case EBEventType.ApplyMadnessAfterMonsterCaught:
+                this.enabled = true;
+                break;
+        }
+    }
+
+    private void Start()
+    {
+        Dispatcher.Subscribe(EBEventType.CaughtByMonster, address, gameObject);
+        Dispatcher.Subscribe(EBEventType.ApplyMadnessAfterMonsterCaught, address, gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        Dispatcher.Unsubscribe(EBEventType.CaughtByMonster, address);
+        Dispatcher.Unsubscribe(EBEventType.ApplyMadnessAfterMonsterCaught, address);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag.Equals("Player"))
