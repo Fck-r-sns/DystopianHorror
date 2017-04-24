fa8abe7e1112af246e133442ac5af90d2034180a
Fck-r-sns <fck.r.sns@gmail.com>
2017-04-24 22:44:13 +0300

    Times caught by monster predicates in negotiation rooms

---
+++ Assets/Scripts/RoomsManagement/Predicates/TimesCaughtByMonsterPredicate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimesCaughtByMonsterPredicate : Predicate
+{
+    [SerializeField]
+    private int lowerBound = 0;
+
+    [SerializeField]
+    private int upperBound = int.MaxValue;
+
+    public override bool Check(WorldState world)
+    {
+        return (lowerBound <= world.timesCaughtByMonster) && (world.timesCaughtByMonster <= upperBound);
+    }
+}
