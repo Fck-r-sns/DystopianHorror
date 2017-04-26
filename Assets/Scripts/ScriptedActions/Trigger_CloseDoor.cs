f6c6d26e33db0960b860dfefcd48ce1799ecfd2b
Fck-r-sns <fck.r.sns@gmail.com>
2017-04-26 23:30:52 +0300

    Fixes and improvements

---
+++ Assets/Scripts/ScriptedActions/Trigger_CloseDoor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Trigger_CloseDoor : MonoBehaviour
+{
+    [SerializeField]
+    private Door door;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        door.Close();
+        door.Lock();
+    }
+}
