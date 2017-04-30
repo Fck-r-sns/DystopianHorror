867fc618e2e312a0e4e515114587027bd0473cbe
Fck-r-sns <fck.r.sns@gmail.com>
2017-05-01 01:14:07 +0300

    Start game by menu button

---
+++ Assets/Scripts/GUI/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private void Start()
+    {
+        SceneManager.LoadScene("Prologue", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Hall", LoadSceneMode.Additive);
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
